Replaces local variance by local modes and various fixes

src/image_processing_pipeline/processes/arithmetic_stack_operation.py
@@ -5,14 +5,16 @@ from image_processing_pipeline.framework.process_step import (
   process_steps,
 )
 
+from image_processing_pipeline._types import Input, Option, Deliverable
+
 
 class ArithmeticStackOperation(AbstractProcessStep):
-  inputs = {"stack_a": np.ndarray, "stack_b": np.ndarray}
-  deliverables = {
-    "result_stack": np.ndarray,
-  }
+  stack_a: Input[np.ndarray]
+  stack_b: Input[np.ndarray]
 
-  options = {"operation": (str, "")}
+  result_stack: Deliverable[np.ndarray]
+
+  operation: Option[str] = ""
 
   def _on_set_inputs(self):
     assert self.stack_a.shape == self.stack_b.shape, "Input stacks must have the same shape"

src/image_processing_pipeline/processes/extract_objects.py
@@ -11,8 +11,8 @@ from image_processing_pipeline._types import Input, RegexDeliverable
 class ExtractObjects(AbstractProcessStep):
   """Extracts a variable, but at execution time constant, number of objects from the stack.
 
-  Currently individual objects must be separated from one another by 0 pixels, but have
-  to have a pixel overlap amongs the stack direction.
+  Currently individual objects must be separated from one another by at least one pixel within each
+  frame. Along the stack, each object must have at least one fixed pixel in each frame.
   """
 
   input_stack: Input[np.ndarray]